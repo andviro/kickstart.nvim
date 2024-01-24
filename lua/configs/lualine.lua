64df75602fda898754300a1400107c6f37dafdc9
Andrew Rodionoff <andviro@gmail.com>
2024-01-25 01:07:37 +0300

    [usability] lualine tweaks

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -1,8 +1,8 @@
 local function imode()
   if vim.o.iminsert == 1 then
-    return [[ru]]
+    return 'ru'
   else
-    return [[en]]
+    return 'en'
   end
 end
 require('lualine').setup {
@@ -11,24 +11,34 @@ require('lualine').setup {
     section_separators = { left = '', right = '' },
     ignore_focus = { 'NvimTree' },
   },
-  -- sections = {
-  --   lualine_a = { imode },
-  --   lualine_b = { 'branch', 'diff' },
-  --   lualine_c = {
-  --     'filename',
-  --     'diagnostics',
-  --     {
-  --       'navic',
-  --       -- Component specific options
-  --       color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
-  --       -- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
-  --       -- Setting it to "static" will perform a adjustment once when the component is being setup. This should
-  --       --   be enough when the lualine section isn't changing colors based on the mode.
-  --       -- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
-  --       --   the current section.
-  --       navic_opts = nil, -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
-  --     },
-  --   },
-  -- },
-  extensions = { 'quickfix', 'man', 'fugitive' },
+  sections = {
+    lualine_a = { imode },
+    lualine_b = { 'branch' },
+    lualine_c = {
+      {
+        'filename',
+        path = 3,
+      },
+      'diff',
+      'diagnostics',
+    },
+    lualine_x = { 'copilot', 'filetype' },
+    lualine_y = { 'progress' },
+    --   lualine_c = {
+    --     'filename',
+    --     'diagnostics',
+    --     {
+    --       'navic',
+    --       -- Component specific options
+    --       color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
+    --       -- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
+    --       -- Setting it to "static" will perform a adjustment once when the component is being setup. This should
+    --       --   be enough when the lualine section isn't changing colors based on the mode.
+    --       -- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
+    --       --   the current section.
+    --       navic_opts = nil, -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
+    --     },
+    --   },
+  },
+  extensions = { 'quickfix', 'man', 'fugitive', 'nvim-tree' },
 }
