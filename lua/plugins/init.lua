f89763b2f9cdbb24ce49c600109d4b6e07db7375
Andrew Rodionoff <andviro@gmail.com>
2024-05-12 10:36:59 +0300

    misc git fixes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,19 +4,6 @@ return {
   'nvim-lua/plenary.nvim',
 
   -- Git related plugins
-  {
-    'tpope/vim-fugitive',
-    config = function()
-      -- map('n', '<leader>gs', '<cmd>Git<cr>', { desc = 'Git status' })
-      -- map('n', '<leader>ga', '<cmd>Git add %<cr>', { desc = 'Git add buffer' })
-      -- map('n', '<leader>gA', '<cmd>Git add -A .<cr>', { desc = 'Git add all' })
-      -- map('n', '<leader>gB', '<cmd>GitBlameToggle<cr>', { desc = 'Toggle Blame' })
-      -- map('n', '<leader>gc', '<cmd>Git commit -v<cr>', { desc = 'Git commit' })
-      -- map('n', '<leader>gC', '<cmd>Git commit -a -v<cr>', { desc = 'Git commit -a' })
-      -- map('n', '<leader>gp', '<cmd>Git push -u origin HEAD<cr>', { desc = 'Git push' })
-      -- map('n', '<leader>gu', '<cmd>Git pull<cr>', { desc = 'Git pull' })
-    end,
-  },
   'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -8,11 +8,11 @@ return {
   config = function()
     local neogit = require 'neogit'
     neogit.setup {}
-    vim.keymap.set('n', '<leader>gs', neogit.open, { silent = true, noremap = true })
-    vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true })
-    vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true })
-    vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { silent = true, noremap = true })
-    vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true })
-    vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>gs', neogit.open, { silent = true, noremap = true, desc = 'Neogit [s]tatus' })
+    -- vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true, desc = 'Neogit [c]ommit' })
+    -- vim.keymap.set('n', '<leader>gu', ':Neogit pull<CR>', { silent = true, noremap = true, desc = 'Neogit p[u]ll' })
+    -- vim.keymap.set('n', '<leader>gp', ':Neogit push<CR>', { silent = true, noremap = true, desc = 'Neogit [p]ush' })
+    vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true, desc = 'Neogit [b]ranches' })
+    -- vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true, desc = 'Git [B]lame' })
   end,
 }
